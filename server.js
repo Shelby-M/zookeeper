cbd826fcaf3f9e49dac857645fa995b049030c36
Your Name <shelbyfayemo@gmail.com>
2021-10-19 15:50:14 -0400

    code

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ function filterByQuery(query, animalsArray) {
   }
   return filteredResults;
 }
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
 
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -29,6 +33,11 @@ app.get("/api/animals", (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  res.json(result);
 });
 
 app.listen(PORT, () => {
